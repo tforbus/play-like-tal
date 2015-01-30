664c996724dcd4dc23f748cb2eeda76d2721f27c
tforbus <tristinforbus@gmail.com>
2015-01-30 14:11:17 -0800

    removed material submodule

--- material/src/components/input/demoErrors/script.js
+++
@@ -1,9 +0,0 @@
-angular.module('inputErrorsApp', ['ngMaterial', 'ngMessages'])
-
-.controller('AppCtrl', function($scope) {
-  $scope.project = {
-    description: 'Nuclear Missile Defense System',
-    clientName: 'Bill Clinton',
-    rate: 500,
-  };
-});
